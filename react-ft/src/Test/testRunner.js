056d1b255f395c59d14100f62e65e3870885256b
hetviprajapati <hetviprajapati114@gmail.com>
2020-02-18 13:35:34 +0530

    ViewQuoteFlowCommit

--- react-ft/src/Test/testRunner.js
+++ react-ft/src/Test/testRunner.js
@@ -1,10 +1,12 @@
 let guestFlow = require('./guestFlow');
+let viewQuoteFlow = require('./viewQuoteFlow');
 let cf=require('./cf');
 
 let pc = cf.sleeping(1000);
 // Execute ALL the FT
 const ExecuteALL = (pc) =>{
-    pc = guestFlow.testGuestFlow(pc);
+  //  pc = guestFlow.testGuestFlow(pc);
+      pc = viewQuoteFlow.testViewQuoteFlow(pc);
     return pc;
 }
 
